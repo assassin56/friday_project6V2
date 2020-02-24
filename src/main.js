f3f600f81eaea4997035e77752d3230ebe1953d4
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-23 18:48:13 -0800

    finalize main.js. Although still unable to output needed information.

--- src/main.js
+++ src/main.js
@@ -12,8 +12,49 @@ $(document).ready(function () {
     const location = $("#inputCity").val();
 
     (async () => {
-      let doc = new Doc();
-      let response = await doc.findDoctor(name, location);
+      let doctor = new Doc;
+      let response = await doctor.findDoctor(name, location);
+
+      fetchElements(response);
+    })(); 
+
+    let fetchElements = function (response) {
+      if (response === false) {
+        $("#output").text('Sorry, there was an error in handling your request.');
+      } else if ( response === 0) {
+        $("ul#docResult").text('There are no results to display at this time.');
+      } else if (response.data.length > 0) {
+        for (let i = 0; i <= response.data.length; i++) {
+          response.data.forEach(function() {
+            $("ul#docResult").append('<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[i].accepts_new_patients} <br> Phone: ${doctor.practices[i].phones[0].number}</li>');
+          });
+        }
+      }
+    }
+  });
+
+  $("form#symptoms").submit(function(event) {
+    event.preventDefault();
+    const currentSymptom = $("#currentSymptom").val();
+
+    (async () => {
+      let doctor = new Doc;
+      let response = await doctor.symptoms(currentSymptom);
+      fetchElements(response);
     })();
+
+    let fetchElements = function (response) {
+      if (response === false) {
+        $("#output").text('Sorry, there was an error in handling your request.');
+      } else if (response.data === 0) {
+        $("ul#docResult").append('There are no results to display at this time.');
+      } else if (response.data.length > 0) {
+        for (let i = 0; i <= response.data.length; i++) {
+          response.data.forEach(function () {
+            $("ul#docResult").append('<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[i].accepts_new_patients} <br> Phone: ${doctor.practices[i].phones[0].number}</li>')
+          }); 
+        }
+      }
+    }
   });
 });

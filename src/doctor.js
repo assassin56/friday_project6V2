7f155d95be1588bccd93143a2121774ed9955e08
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-23 17:07:38 -0800

    add methods to fetch api response.

--- src/doctor.js
+++ src/doctor.js
@@ -1,5 +1,34 @@
 import './../src/main.js';
 
 export class Doc {
-  
+  async findDoctor() {
+    try {
+      let response = await fetch('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}');
+
+      if (response.ok && response.status === 200) {
+        let postJSON = await response.json();
+        return postJSON;
+      } else {
+        postJSON = false;
+      }
+    } catch(error) {
+      return false;
+    }
+  }
+
+
+  async symptoms() {
+    try {
+      let response = await fetch ('https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}');
+
+      if (response.ok && response.status === 200) {
+        let postJSON = await response.json();
+        return postJSON;
+      } else {
+        postJSON = false;
+      }
+    } catch(error) {
+      return false;
+    }
+  }
 }

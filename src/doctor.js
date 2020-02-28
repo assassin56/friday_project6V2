8e26a96a270b8c7578b6894dd8d32b28cec9f85d
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-02-28 07:49:32 -0800

    include radius in Portland search.

--- src/doctor.js
+++ src/doctor.js
@@ -3,7 +3,7 @@ import './../src/main.js';
 export class Doc {
   async findDoctor (name) {
     try {
-      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.505%2C%20-122.675&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.505%2C%20-122.675%2C%20100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
 
       if (response.ok && response.status === 200) {
         let postJSON = await response.json();
@@ -19,7 +19,7 @@ export class Doc {
 
   async symptoms(symptoms) {
     try {
-      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?&query=${symptoms}&location=45.505%2C%20-122.675&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+      let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?&query=${symptoms}&location=45.505%2C%20-122.675%2C%20100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=${process.env.API_KEY}`);
 
       if (response.ok && response.status === 200) {
         let postJSON = await response.json();
--- src/main.js
+++ src/main.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
         $("ul#docResult").text('There are no results to display at this time.');
       } else if (response.data.length > 0) {
         for (let i = 0; i <= response.data.length; i++) {
-          response.data.forEach(function(doctor) {
+          response.data.forEach(function (doctor) {
             $("ul#docResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[i].visit_address.city} <br> ${doctor.practices[i].visit_address.state} <br> ${doctor.practices[i].visit_address.street} <br> ${doctor.practices[i].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[i].accepts_new_patients} <br> Phone: ${doctor.practices[i].phones[0].number}</li>`);
           });
         }

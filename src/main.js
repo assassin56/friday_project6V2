402632258b4eb2494dd575db172b252031e9f471
Antonio Cruz <codrocks.cruz3@gmai.com>
2020-03-01 13:10:13 -0800

    fix 'undefined' message for website call.

--- src/main.js
+++ src/main.js
@@ -23,9 +23,11 @@ $(document).ready(function () {
       } else if ( response.data.length === 0) {
         $("ul#docResult").text('There are no results to display at this time.');
       } else if (response.data.length > 0) {
+        $("ul#docResult").empty();
           response.data.forEach(function (doctor) {
-            $("ul#docResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[0].visit_address.city} <br> ${doctor.practices[0].visit_address.state} <br> ${doctor.practices[0].visit_address.street} <br> ${doctor.practices[0].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[0].accepts_new_patients} <br> Phone: ${doctor.practices[0].phones[0].number} <br> ${doctor.practices[0].website} </li>`);
-            if (doctor.practices[0].website === undefined) {
+            let website = doctor.practices[0].website || "";
+            $("ul#docResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[0].visit_address.city} <br> ${doctor.practices[0].visit_address.state} <br> ${doctor.practices[0].visit_address.street} <br> ${doctor.practices[0].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[0].accepts_new_patients} <br> Phone: ${doctor.practices[0].phones[0].number} <br> ${website} </li>`);
+            if (website === "") {
               return $("ul#docResult").append("This website information is not available");
             }
           });
@@ -49,9 +51,11 @@ $(document).ready(function () {
       } else if (response.data.length === 0) {
         $("ul#docResult").append('There are no results to display at this time.');
       } else if (response.data.length > 0) {
+        $("ul#docResult").empty();
           response.data.forEach(function (doctor) {
-            $("ul#docResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[0].visit_address.city} <br> ${doctor.practices[0].visit_address.state} <br> ${doctor.practices[0].visit_address.street} <br> ${doctor.practices[0].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[0].accepts_new_patients} <br> Phone: ${doctor.practices[0].phones[0].number} ${doctor.practices[0].website} </li>`);
-            if (doctor.practices[0].website === undefined) {
+            let website = doctor.practices[0].website || "";
+            $("ul#docResult").append(`<li>${doctor.profile.first_name} ${doctor.profile.last_name} <br> ${doctor.practices[0].visit_address.city} <br> ${doctor.practices[0].visit_address.state} <br> ${doctor.practices[0].visit_address.street} <br> ${doctor.practices[0].visit_address.zip} <br>  Accepts new patients: ${doctor.practices[0].accepts_new_patients} <br> Phone: ${doctor.practices[0].phones[0].number} <br> ${website} </li>`);
+            if (website == "") {
               return $("ul#docResult").append("This website information is not available");
             }
           }); 
